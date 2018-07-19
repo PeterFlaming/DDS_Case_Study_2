01c69d33bd6e8b12f8071869bc9ac4bd41a3b069
qatsmu <qnixon@mail.smu.edu>
2018-07-19 10:19:25 -0700

    First commit
    Importing sample files for exploration.  The two “setwd()” commands
    allow switching between the file structure on my computer to store
    results in my own folder while pulling data from the main branch folder.

---
+++ qn_code_and_plots/qn_initial_EDA.R
@@ -0,0 +1,6 @@
+# setwd("~/Documents/@Grad School/@SMU/@Term 1 Courses/Doing Data Science/Week11/DDS_Case_Study_2/qn_code_and_plots")
+# setwd("~/Documents/@Grad School/@SMU/@Term 1 Courses/Doing Data Science/Week11/DDS_Case_Study_2/data")
+
+
+well_data <- read.csv("deo_well_data.csv", stringsAsFactors = FALSE)
+registry_data <- read.csv("fracfocus_registry_texas.csv", stringsAsFactors = FALSE)
