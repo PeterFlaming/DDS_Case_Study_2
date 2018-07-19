a6c538f07ab2322688b3bd95b7e2b99257242e5e
qatsmu <qnixon@mail.smu.edu>
2018-07-19 10:39:25 -0700

    Boxplots of peak production

--- qn_code_and_plots/qn_initial_EDA.R
+++ qn_code_and_plots/qn_initial_EDA.R
@@ -4,3 +4,11 @@
 
 well_data <- read.csv("deo_well_data.csv", stringsAsFactors = FALSE)
 registry_data <- read.csv("fracfocus_registry_texas.csv", stringsAsFactors = FALSE)
+
+well_data$FirstProd <- as.Date(well_data$FirstProd, "%m/%d/%Y")
+well_data$Oil_PkNorm_PerK_6mo <- as.numeric(well_data$Oil_PkNorm_PerK_6mo)
+
+
+peak_performance_wells <- subset(well_data, !is.na(well_data$Oil_PkNorm_PerK_6mo))
+
+boxplot(log(peak_performance_wells$Oil_PkNorm_PerK_6mo))
