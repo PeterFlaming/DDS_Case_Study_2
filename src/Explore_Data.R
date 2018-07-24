b4c6ac7de937ed96e584ab2f24b87a0cff175c4b
Brock Friedrich <brocklfriedrich@gmail.com>
2018-07-24 18:25:52 -0500

    pushing for work session

---
+++ src/Explore_Data.R
@@ -0,0 +1,14 @@
+ggplot2::theme_set(ggplot2::theme_bw())
+ggplot2::theme_update(plot.title = ggplot2::element_text(hjust = 0.5))
+knitr::opts_chunk$set(fig.width = 12,
+                      fig.height = 8,
+                      fig.path = 'Figs/',
+                      warning = FALSE,
+                      message = FALSE)
+
+require(tidyverse)
+require(magrittr)
+require(kableExtra)
+options(knitr.table.format = "markdown")
+
+setwd("C:/Repositories/DDS_Case_Study_2")
