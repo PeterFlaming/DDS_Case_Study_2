3c76990b6687a056c0c856b409c40140fa287edf
qatsmu <qnixon@mail.smu.edu>
2018-07-27 06:11:01 -0700

    Adding Peak Performance Plots
    plotting pairs of variables to see how things are correlated

--- src/qn_code_and_plots/qn_initial_EDA.R
+++ src/qn_code_and_plots/qn_initial_EDA.R
@@ -3,17 +3,18 @@ setwd("~/Documents/@Grad School/@SMU/@Term 1 Courses/Doing Data Science/Week11/D
 
 library(tidyverse)
 
-
+# original import
 well_data <- read.csv("deo_well_data.csv", stringsAsFactors = FALSE)
 registry_data <- read.csv("fracfocus_registry.csv", stringsAsFactors = FALSE)
 
-well_data$FirstProd <- as.Date(well_data$FirstProd, "%m/%d/%Y")
-well_data$Oil_PkNorm_PerK_6mo <- as.numeric(well_data$Oil_PkNorm_PerK_6mo)
-well_data$Oil_PkNorm_PerK_6mo <- as.integer(well_data$Oil_PkNorm_PerK_6mo)
-
-peak_performance_wells <- subset(well_data, !is.na(well_data$Oil_PkNorm_PerK_6mo))
-boxplot(log(peak_performance_wells$Oil_PkNorm_PerK_6mo))
-barplot(peak_performance_wells$oper_alias)
+# old plots Do Not Use
+# well_data$FirstProd <- as.Date(well_data$FirstProd, "%m/%d/%Y")
+# well_data$Oil_PkNorm_PerK_6mo <- as.numeric(well_data$Oil_PkNorm_PerK_6mo)
+# well_data$Oil_PkNorm_PerK_6mo <- as.integer(well_data$Oil_PkNorm_PerK_6mo)
+# 
+# peak_performance_wells <- subset(well_data, !is.na(well_data$Oil_PkNorm_PerK_6mo))
+# boxplot(log(peak_performance_wells$Oil_PkNorm_PerK_6mo))
+# barplot(peak_performance_wells$oper_alias)
 
 # pulling code chunk from Brock's file for consistency
 driftwood <- well_data %>%
@@ -55,3 +56,35 @@ fracfocus <- registry_data %>%
           ,MassIngredient
           ,IsWater
   )
+
+# peak performance plots
+peak_performance_wells <- subset(driftwood, !is.na(driftwood$Oil_PkNorm_Perk_6mo))
+boxplot(peak_performance_wells$Oil_PkNorm_Perk_6mo)
+boxplot(log(peak_performance_wells$Oil_PkNorm_Perk_6mo), main = "Log of Peak Performance")
+# wells by operator 
+wells_by_operator <- peak_performance_wells %>% count(oper_alias) %>% arrange(desc(n))
+colnames(wells_by_operator) <- c("operator", "well_count")
+wells_by_operator
+
+operator_plot <- ggplot(wells_by_operator, aes(reorder(operator, well_count),
+                                               well_count, fill = operator))
+operator_plot + geom_col() + ggtitle("Wells by Operator") + coord_flip() +
+  theme(legend.position="none")
+# these clusters might be worth exploring further
+plot(peak_performance_wells$PerfLL, peak_performance_wells$Oil_PkNorm_Perk_6mo,
+     main = "Peak Performance vs PerfLL")
+# do newer wells have higher peaks?  maybe
+plot(peak_performance_wells$FirstProd, peak_performance_wells$Oil_PkNorm_Perk_6mo,
+     main = "Peak Performance by Start Date")
+# do newer wells have more PerfLL or are there just more newer wells
+plot(peak_performance_wells$FirstProd, peak_performance_wells$PerfLL,
+     main = "PerfLL by Start Date")
+wells_by_date <- peak_performance_wells %>%
+  group_by(FirstProd) %>%
+  summarise(wells_this_year = n_distinct(API))
+# drilling wells has definitely increased in recent years
+plot(wells_by_date$FirstProd, wells_by_date$wells_this_year, 
+     main = "Wells by Date")
+
+
+
