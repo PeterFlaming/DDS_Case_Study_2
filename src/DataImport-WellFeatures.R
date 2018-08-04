7d356f92755acc1ae56e4a6e7cb5305bbe376f37
Brock Friedrich <brocklfriedrich@gmail.com>
2018-08-04 15:14:42 -0500

    fixed file refs. working on well import

--- src/DataImport-WellFeatures.R
+++ src/DataImport-WellFeatures.R
@@ -1,2 +1,63 @@
+#setwd('C:\\Repositories\\DDS_Case_Study_2\\src')
+source('Setup.R')
+source('Functions.R')
+
+well_data <- read.csv("../data/deo_well_data.csv") %>%
+             standardize_names() %>% 
+             rename(oil='oilpknormperk6mo') %>%
+             mutate(api = as.character(api)
+                     , api10 = as.character(api10)
+                     , perfll=as.integer(perfll)
+                     , firstprod = as.Date(firstprod, "%m/%d/%Y")
+                     , oil = as.integer(oil)
+                     , firstprod_year = as.integer(format(firstprod, '%Y'))
+                     ) %>%
+             as.tibble()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
